db6ced403b22ba1067ef7246b2e5d0cb5793d06a
frank MBTECH <frank@musabaytechnologies.com>
2021-08-07 15:33:31 -0400

    This is john, and i am adding new automation code.

---
+++ Calculator2/src/main/java/com/calculater2/practice/JohnHello.java
@@ -0,0 +1,11 @@
+package com.calculater2.practice;
+
+public class JohnHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi Team, I am started working today. and feeling very happy !");
+
+	}
+
+}
